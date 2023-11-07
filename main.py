b1b582e27095376da58ef3821fa3623c4295958b
uchansansan <uchansansan@gmail.com>
2023-11-07 11:31:03 +0700

    algos

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-input = open('input.txt', 'r')
+input = open('input.txt', 'r', encoding='utf-8')
 
 num_sen = int(input.readline())
 
-with open('input.txt', 'r') as file_in:
+with open('input.txt', 'r', encoding='utf-8') as file_in:
     text = file_in.readlines()
 words = []
 
@@ -11,11 +11,14 @@ for i in text:
 
 words = words[1:]
 
-print(words)
 
 words_dict = {}
 
 for idx in range(len(words)-1):
-    words_dict[words[idx]] = words[idx+1]
+    if words[idx] in words_dict.keys():
+        words_dict[words[idx]] += [words[idx+1]]
+    else:
+        words_dict[words[idx]] = [words[idx + 1]]
 
 print(words_dict)
+
